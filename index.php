91faca7c5d5f2a7ecbb7c28ecb7dd523b09bf878
briannzw <brianwijaya2003@gmail.com>
2021-12-14 16:13:18 +0700

    Update Website Logo

--- index.php
+++ index.php
@@ -24,7 +24,8 @@
 <html>
 
 <head>
-    <title>Project UAS Semantic Web - 201401042</title>
+    <title>Project UAS Lab Semantic Web - 201401042</title>
+    <link rel="icon" href="img/logo.png">
     <!--Fonts-->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
--- result.php
+++ result.php
@@ -222,6 +222,7 @@
 
 <head>
     <title>Search Results : <?php if($title != "") : echo $title . " Games"; elseif($search_uri != "") : echo $result[0]->label; else : echo "[GENRE]"; endif; ?></title>
+    <link rel="icon" href="img/logo.png">
     <!--Fonts-->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
